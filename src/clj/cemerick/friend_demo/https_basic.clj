17faa89080095a7d3e2cfc7e2699ed752bb7cdee
Chas Emerick <chas@cemerick.com>
2012-12-13 09:31:13 -0500

    https-basic placeholder

---
+++ src/clj/cemerick/friend_demo/https_basic.clj
@@ -0,0 +1,3 @@
+(ns ^{:name "HTTP Basic over SSL (\"channel security\" middleware)"
+      :doc "Same as 'HTTP Basic', but with the added condition that HTTPS/SSL is used (suitable for web service APIs)."}
+  cemerick.friend-demo.https-basic)
