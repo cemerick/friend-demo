909a18ec7d3d02ada71f9bea8961961e12dfbd6b
Chas Emerick <chas@cemerick.com>
2013-03-29 15:40:01 -0400

    upgrade to friend 0.1.5

--- project.clj
+++ project.clj
@@ -8,7 +8,7 @@
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.4.0"]
                  
-                 [com.cemerick/friend "0.1.5-SNAPSHOT"]
+                 [com.cemerick/friend "0.1.5"]
 
                  [compojure "1.1.4"]
                  [ring/ring-jetty-adapter "1.1.0"]
