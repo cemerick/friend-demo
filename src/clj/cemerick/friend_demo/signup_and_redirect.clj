8fc7e54e2f25fe7e27db7f9494cfd8aa91c9598f
Chas Emerick <chas@cemerick.com>
2013-06-21 10:50:52 -0400

    add admin control to signup-and-redirect, minor tidying

--- src/clj/cemerick/friend_demo/signup_and_redirect.clj
+++ src/clj/cemerick/friend_demo/signup_and_redirect.clj
@@ -10,27 +10,32 @@
             (compojure [handler :as handler]
                        [route :as route])
             [ring.util.response :as resp]
+            [clojure.string :as str]
             [hiccup.page :as h]
             [hiccup.element :as e]))
 
 (defn- create-user
-  [& {:keys [username password]}]
-  {:username username
-   :password (creds/hash-bcrypt password)
-   :identity username
-   :roles #{::users/user}})
+  [{:keys [username password admin] :as user-data}]
+  (-> (dissoc user-data :admin)
+      (assoc :identity username
+             :password (creds/hash-bcrypt password)
+             :roles (into #{::users/user} (when admin [::users/admin])))))
 
 (defn- signup-form
   [flash]
   [:div {:class "row"}
    [:div {:class "columns small-12"}
-    [:h3 "Sign up " [:small "(Any user/pass combination will do, as you are creating a new account or profile.)"]]
+    [:h3 "Sign up "
+     [:small "(Any user/pass combination will do, as you are creating a new account or profile.)"]]
     [:div {:class "row"}
      [:form {:method "POST" :action "signup" :class "columns small-4"}
       [:div "Username" [:input {:type "text" :name "username" :required "required"}]]
       [:div "Password" [:input {:type "password" :name "password" :required "required"}]]
       [:div "Confirm" [:input {:type "password" :name "confirm" :required "required"}]]
-      [:div [:input {:type "submit" :class "button" :value "Sign up"}] [:span {:style "padding:0 0 0 10px;color:red;"} flash]]]]]])
+      [:div "Make you an admin? " [:input {:type "checkbox" :name "admin"}]]
+      [:div
+       [:input {:type "submit" :class "button" :value "Sign up"}]
+       [:span {:style "padding:0 0 0 10px;color:red;"} flash]]]]]])
 
 (def login-form
   [:div {:class "row"}
@@ -53,7 +58,7 @@
        [:h3 "Current Status " [:small "(this will change when you log in/out)"]]
        [:p (if-let [identity (friend/identity req)]
              (apply str "Logged in, with these roles: "
-               (-> identity friend/current-authentication :roles))
+               (-> identity friend/current-authentication :roles pr-str))
              "anonymous user")]
        (signup-form (:flash req))
        [:h3 "Authorization demos"]
@@ -69,18 +74,15 @@
        [:p (e/link-to (misc/context-uri req "logout") "Click here to log out") "."])))
   (GET "/login" req
     (h/html5 misc/pretty-head (misc/pretty-body login-form)))
-  (POST "/signup" req
-       (let [username (:username (req :params))
-             password (:password (req :params))
-             confirm (:confirm (req :params))]
-         (if (and (not-any? clojure.string/blank? [username password confirm])
-                  (= password confirm))
-           (let [user (create-user :username username
-                                   :password password)]
-             (friend/merge-authentication
-               (resp/redirect (misc/context-uri req username))
-               user))
-           (assoc (resp/redirect (str (:context req) "/")) :flash "passwords don't match!"))))
+  (POST "/signup" {{:keys [username password confirm] :as params} :params :as req}
+        (if (and (not-any? str/blank? [username password confirm])
+                 (= password confirm))
+          (let [user (create-user (select-keys params [:username :password :admin]))]
+            ;; HERE IS WHERE YOU'D PUSH THE USER INTO YOUR DATABASES if desired
+            (friend/merge-authentication
+              (resp/redirect (misc/context-uri req username))
+              user))
+          (assoc (resp/redirect (str (:context req) "/")) :flash "passwords don't match!")))
   (GET "/logout" req
     (friend/logout* (resp/redirect (str (:context req) "/")) ))
   (GET "/requires-authentication" req
